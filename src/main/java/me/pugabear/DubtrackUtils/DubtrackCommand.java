d2ef55a032b3fc632c43a5ea1610b60f4cd995be
PugaBear <pugabear@bn-mc.net>
2017-04-11 12:50:05 -0500

    Apparently this is how you format switches

--- src/main/java/me/pugabear/DubtrackUtils/DubtrackCommand.java
+++ src/main/java/me/pugabear/DubtrackUtils/DubtrackCommand.java
@@ -32,101 +32,100 @@ public class DubtrackCommand implements CommandExecutor {
 		}
 		try {
 			switch (level) {
-				case "admin":
-					switch (first) {
-						case "reload": 
-							Utils.loadConfig();
-							break;
-						case "reconnect":
-							dta.getAPI().logout();
-							Utils.getRoomId();
-							dta.init();
-							dta.register();
-							break;
-						case "reset":
-							dta.getAPI().logout();
-							Utils.loadConfig();
-							Utils.getRoomId();
-							dta.init();
-							dta.register();
-							break;
-						default:
-							sender.sendMessage(prefix + Utils.color("lang.admin.usage"));
-							return true;
-					}
-					sender.sendMessage(prefix + Utils.color("lang.admin." + args[0]));
+			case "admin":
+				switch (first) {
+				case "reload": 
+					Utils.loadConfig();
+					break;
+				case "reconnect":
+					dta.getAPI().logout();
+					Utils.getRoomId();
+					dta.init();
+					dta.register();
+					break;
+				case "reset":
+					dta.getAPI().logout();
+					Utils.loadConfig();
+					Utils.getRoomId();
+					dta.init();
+					dta.register();
+					break;
+				default:
+					sender.sendMessage(prefix + Utils.color("lang.admin.usage"));
 					return true;
-				case "mod":
-					switch (first) {
-						case "ban":
-							dta.getRoom().banUser(args[1]);
-							sender.sendMessage(prefix + Utils.color("lang.mod.ban").replaceAll("%user%", args[1]));
-							return true;
-						case "unban":
-							dta.getRoom().unbanUser(args[1]);
-							sender.sendMessage(prefix + Utils.color("lang.mod.unban").replaceAll("%user%", args[1]));
-							return true;
-						case "kick":
-							dta.getRoom().kickUser(args[1]);
-							sender.sendMessage(prefix + Utils.color("lang.mod.kick").replaceAll("%user%", args[1]));
-							return true;
-						case "skip":
-							dta.getRoom().skipSong();
-							sender.sendMessage(prefix + Utils.color("lang.mod.skip"));
-							return true;
-						default:
-							sender.sendMessage(prefix + Utils.color("lang.mod.usage"));
-							return true;
+				}
+				sender.sendMessage(prefix + Utils.color("lang.admin." + args[0]));
+				return true;
+			case "mod":
+				switch (first) {
+				case "ban":
+					dta.getRoom().banUser(args[1]);
+					break;
+				case "unban":
+					dta.getRoom().unbanUser(args[1]);
+					break;
+				case "kick":
+					dta.getRoom().kickUser(args[1]);
+					break;
+				case "skip":
+					dta.getRoom().skipSong();
+					sender.sendMessage(prefix + Utils.color("lang.mod.skip"));
+					return true;
+				default:
+					sender.sendMessage(prefix + Utils.color("lang.mod.usage"));
+					return true;
+				}
+				sender.sendMessage(prefix + Utils.color("lang.mod." + first).replaceAll("%user%", args[1]));
+				return true;
+			case "use":
+				switch (first) {
+				case "mute":
+				case "hide":
+					// Hide dubtrack messages from player
+				default:
+					String url = "https://dubtrack.fm/join/" + config.getString("settings.room");
+					String info = config.getString("lang.info")
+							.replaceAll("&", "§");
+
+					String join = config.getString("lang.join")
+							.replaceAll("&", "§")
+							.replaceAll("%url%", url);
+
+					String song = dta.getRoom().getCurrentSong().getSongInfo().getName();
+					String dj = dta.getRoom().getCurrentSong().getUser().getUsername();
+					
+					String display = config.getString("lang.display")
+							.replaceAll("&", "§")
+							.replaceAll("%song%", song)
+							.replaceAll("%dj%", dj);
+
+					String cp = config.getString("lang.currentlyplaying")
+							.replaceAll("&", "§")
+							.replaceAll("%display%", display)
+							.replaceAll("%song%", song)
+							.replaceAll("%dj%", dj);
+
+					if (!info.isEmpty()) {
+						String[] infoSplit = info.split("%new%");
+						for (String _info : infoSplit) {
+							sender.sendMessage(_info);
+						}
 					}
-				case "use":
-					switch (first) {
-						case "mute":
-						case "hide":
-							// Hide dubtrack messages from player
-						default:
-							String url = "https://dubtrack.fm/join/" + config.getString("settings.room");
-							String info = config.getString("lang.info")
-									.replaceAll("&", "§");
-		
-							String join = config.getString("lang.join")
-									.replaceAll("&", "§")
-									.replaceAll("%url%", url);
-		
-							String song = dta.getRoom().getCurrentSong().getSongInfo().getName();
-							String dj = dta.getRoom().getCurrentSong().getUser().getUsername();
-							
-							String display = config.getString("lang.display")
-									.replaceAll("&", "§")
-									.replaceAll("%song%", song)
-									.replaceAll("%dj%", dj);
-		
-							String cp = config.getString("lang.currentlyplaying")
-									.replaceAll("&", "§")
-									.replaceAll("%display%", display)
-									.replaceAll("%song%", song)
-									.replaceAll("%dj%", dj);
-		
-							if (!info.isEmpty()) {
-								String[] infoSplit = info.split("%new%");
-								for (String _info : infoSplit) {
-									sender.sendMessage(_info);
-								}
-							}
-		
-							if (!join.isEmpty()) {
-								String[] joinSplit = join.split("%new%");
-								for (String _join : joinSplit) {
-									sender.sendMessage(_join);
-								}
-							}
-		
-							if (!cp.isEmpty()) {
-								String[] cpSplit = cp.split("%new%");
-								for (String _cp : cpSplit) {
-									sender.sendMessage(_cp);
-								}
-							}
+
+					if (!join.isEmpty()) {
+						String[] joinSplit = join.split("%new%");
+						for (String _join : joinSplit) {
+							sender.sendMessage(_join);
+						}
 					}
+
+					if (!cp.isEmpty()) {
+						String[] cpSplit = cp.split("%new%");
+						for (String _cp : cpSplit) {
+							sender.sendMessage(_cp);
+						}
+					}
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
